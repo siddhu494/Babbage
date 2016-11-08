47a261960b2efd2e04c6743c9d4dfe3e19f0195b
siddhu494 <smudigonda@loginsoft.com>
2016-11-08 18:13:08 +0530

    Update Babbage1.java

--- babbage/src/babbage/Babbage1.java
+++ babbage/src/babbage/Babbage1.java
@@ -5,6 +5,7 @@ public class Babbage1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("Initial commit");
+		 System.out.println("Initial commit2");
 	}
 
 }
